7ca85546196e3b7e4c9867ecf0adb0057464f16a
Laysan Nova <laysansh@gmail.com>
2024-05-05 20:31:10 -0600

    allure

--- playwright.config.js
+++ playwright.config.js
@@ -22,10 +22,10 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
+    // ['html'],
     ['allure-playwright'],
-    ['list'],
-    ['dot']
+    // ['list'],
+    // ['dot']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
